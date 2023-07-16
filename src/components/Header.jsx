7721290685054cc21935f0ba9a1abaf02ed68834
Mohammed Azam <33384508+mohammedazam55@users.noreply.github.com>
2023-07-17 01:43:34 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,12 +20,14 @@ export default function Header() {
                   </a>
                   <a
                     href="http:"
+                    disabled
                     className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                   >
                     Rockets
                   </a>
                   <a
                     href="http:"
+                    disabled
                     className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                   >
                     Launches
